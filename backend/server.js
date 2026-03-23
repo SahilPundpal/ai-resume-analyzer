61233283d02669caf4c4876f5f68e35b87d251f4
Sahil Pundpal <pundpalsahil@gmail.com>
2026-03-23 19:29:14 +0530

    fixing backend

--- backend/server.js
+++ backend/server.js
@@ -312,7 +312,7 @@ app.use((err, req, res, next) => {
 
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`\n✅ Server running on http://localhost:${PORT}`);
